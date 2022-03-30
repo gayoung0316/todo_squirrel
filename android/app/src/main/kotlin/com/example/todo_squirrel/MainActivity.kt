e4bed195db7769242fa5eff28f2605b1417e3572
gayoung0316 <baekmoon1230@gmail.com>
2022-03-30 23:01:52 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/todo_squirrel/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.todo_squirrel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/onboarding.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class OnboardingPage extends StatefulWidget {
+  @override
+  State<OnboardingPage> createState() => _OnboardingPageState();
+}
+
+class _OnboardingPageState extends State<OnboardingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
