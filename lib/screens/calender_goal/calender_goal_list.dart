1750984c752d2a396afca463cf2f1c3eecbd11d8
gayoung0316 <baekmoon1230@gmail.com>
2022-06-30 11:33:36 +0900

    코드 리팩토링

--- lib/screens/calender_goal/calender_goal_list.dart
+++ lib/screens/calender_goal/calender_goal_list.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:sliding_up_panel/sliding_up_panel.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:todo_squirrel/model/squirrel_character.dart';
 import 'package:todo_squirrel/providers/calender_goal_check_provider.dart';
